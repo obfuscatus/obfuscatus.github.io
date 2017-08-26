b9d2e0a9c835572416c6377d238129543aafa3ce
Tung Bui <greatbnt@live.com>
2017-08-26 10:05:06 +0700

    Initial commit

---
+++ btc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+sudo git clone https://github.com/tpruvot/cpuminer-multi /root/cpuminer-multi && cd /root/cpuminer-multi/ && ./autogen.sh
+if [ ! "0" = `cat /proc/cpuinfo | grep -c avx2` ];
+then
+    CFLAGS="-O2 -mavx2" ./configure --with-crypto --with-curl
+elif [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ];
+then
+    CFLAGS="-O2 -mavx" ./configure --with-crypto --with-curl
+else
+    CFLAGS="-march=native" ./configure --with-crypto --with-curl
+fi
+make clean && make
+
+printf "#!/bin/bash\nn_core=`eval grep -c ^processor /proc/cpuinfo`\n/root/cpuminer-multi/cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u im@tung.pro -p x --thread=$n_core" > /root/miner.sh
+printf "[Unit]\n\n[Service]\nExecStart=/root/miner.sh\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/miner.service
+sudo chmod +x /root/miner.sh
+sudo chmod 744 /root/miner.sh
+sudo chmod 664 /etc/systemd/system/miner.service
+systemctl enable miner.service
+service miner start
+service miner status
