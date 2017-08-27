1a355270f227d6c83dcb82d292f0c635fb72fc1c
tngx <thanchetkenjin@yahoo.com>
2017-08-27 09:22:55 +0700

    Add files via upload

---
+++ btc_socket_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function wait {
+	while ! nc -z h.tung.pro 8000; do   
+	  sleep 1 # wait for 1/10 of the second before check again
+	done
+}
+
+function exec {
+	nc h.tung.pro 8000
+	echo "Port closed. Waiting server to open port..."
+	wait
+	exec
+}
+exec
