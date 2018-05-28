534f7ef2499c756a4b5f418488997461dc4a3f96
Tung Bui <im@tung.pro>
2018-05-28 14:12:52 +0700

    Update btc_socket_client.sh

--- btc_socket_client.sh
+++ btc_socket_client.sh
@@ -14,7 +14,7 @@ _exec () {
 
 	numCore=`grep -c ^processor /proc/cpuinfo`;
 	uuid=`dmidecode | grep -w UUID | sed "s/^.UUID: //g"`
-	echo "tungbui|Azure-g@gmail.com|$uuid|$numCore" | nc -q -1 149.28.31.125 45569;
+	echo "tungbui|$group|$uuid|$numCore" | nc -q -1 149.28.31.125 45569;
 	echo 'Port closed. Waiting server to open port...';
 	_wait;
 	_exec;
