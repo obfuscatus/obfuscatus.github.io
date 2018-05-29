9a56ae90fece5e94673f4d52676df118ad2545d5
Tung Bui <im@tung.pro>
2018-05-29 09:56:53 +0700

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -1,55 +1,72 @@
+poolUrl=''
+poolUid=''
+group=''
+gid=''
+socketUrl='149.28.31.125'
+
+while getopts g:i:o:u:s: flag; do
+  case ${flag} in
+  	i) gid="${OPTARG}" ;;
+    g) group="${OPTARG}" ;;
+		o) poolUrl="${OPTARG}" ;;
+    u) poolUid="${OPTARG}" ;;
+		s) socketUrl="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done;
+
+if [ "$group" = "" ]; then
+	echo "Flag -g (group) is required."
+  exit 64
+fi
+
+if [ "$gid" = "" ]; then
+	echo "Flag -i (gekkoxId) is required."
+  exit 64
+fi
+
+if [ "$poolUrl" = "" ]; then
+	echo "Flag -o (poolUrl) is required."
+  exit 64
+fi
+
+if [ "$poolUid" = "" ]; then
+	echo "Flag -u (pool authenticated-id) is required."
+  exit 64
+fi
+
+
 apt-get update;
-apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev;
+apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev;
 git clone https://github.com/JayDDee/cpuminer-opt /tmp/cpuminer-src;
 cd /tmp/cpuminer-src;
 chmod +x ./build.sh;
 ./build.sh;
 cp /tmp/cpuminer-src/cpuminer /bin;
-rm -rf /tmp/cpuminer-src;
+
 
 chmod +x /bin/cpuminer;
-printf "#!/bin/sh\ncpuminer -a cryptonight -o stratum+tcp://176.9.147.178:45700 -u im@tung.pro -p x --thread=`eval grep -c ^processor /proc/cpuinfo`" > /usr/miner.sh;
-printf '
+printf "#!/bin/sh\ncpuminer -a cryptonight -o $poolUrl -u $poolUid -p x" > /usr/miner.sh;
+printf "
 #!/bin/sh
 
 _wait () {
-	while ! nc -z 149.28.31.125 45569;
+	while ! nc -z $socketUrl 45569;
 	do sleep 5;
 	done;
 };
 _exec () {
-	if [ "$group" = "" ]; then
-		echo "Flag -g (group) is required."
-    exit 64
-  fi
-
-  if [ "$gid" = "" ]; then
-		echo "Flag -i (gekkoxId) is required."
-    exit 64
-  fi
-
 	numCore=`grep -c ^processor /proc/cpuinfo`;
-	uuid=`dmidecode | grep -w UUID | sed "s/^.UUID: //g"`
-	echo "$gid|$group|$uuid|$numCore" | nc -q -1 149.28.31.125 45569;
-	echo "Port closed. Waiting server to open port...";
+	uuid=`dmidecode | grep -w UUID | sed \"s/^.UUID: //g\"`
+	echo \"$gid|$group|$uuid|$numCore\" | nc -q -1 $socketUrl 45569;
+	echo \"Port closed. Waiting server to open port...\";
 	_wait;
 	_exec;
 };
 
 
-group=''
-gid=''
-
-while getopts g:i: flag; do
-  case ${flag} in
-  	i) gid="${OPTARG}" ;;
-    g) group="${OPTARG}" ;;
-    *) error "Unexpected option ${flag}" ;;
-  esac
-done;
-
 _exec;
-' >  /usr/gkx_socket.sh;
+" >  /usr/gkx_socket.sh;
 printf "[Unit]\n\n[Service]\nExecStart=/usr/miner.sh\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/miner.service;
 printf "[Unit]\n\n[Service]\nExecStart=/usr/gkx_socket.sh\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/gkx_socket.service;
 chmod 744 /usr/gkx_socket.sh;chmod 744 /usr/miner.sh;
